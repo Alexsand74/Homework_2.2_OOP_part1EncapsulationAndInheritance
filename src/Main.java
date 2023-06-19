e910405113b5653524c4ecbce429ac937a6270ab
Alexsand74 <1974alexsand@gmail.com>
2023-06-19 23:18:07 +0300

    код находится в ветке, домашнее задание - 2.2 ООП часть 1. Инкапсуляция и наследование

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("code is in branch, homework " +
+                "- 2.2 OOP part 1. Encapsulation and inheritance");
+    }
+}
